918289bcd006a4f423d9c79118e5b188d128b5b0
ibrahimtezvergi <tezvergil.ibrahim@gmail.com>
2020-12-17 20:32:59 +0000

    odev tamamladı

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
-function Animal(name){
-    this.name = name;
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    action(e) {
+        document.getElementById("animalImage").style.display="block";
+        document.getElementById(this.actionSoundName).play();
+
+    }
+
+    showImage() {
+        document.getElementById("animalImage").src = this.image;
+
+    }
+
+    putInTheDocument() {
+        
+        var petsTable = document.getElementById("petsTable");
+        var petTR = document.createElement("tr");
+
+        var petNameTD = document.createElement("td");
+        petNameTD.textContent = this.name;
+        petTR.appendChild(petNameTD);
+
+        var petLegsTD = document.createElement("td");
+        petLegsTD.textContent = this.legs;
+        petTR.appendChild(petLegsTD);
+
+        var petActionTD = document.createElement("td");
+        var petActionTDButton = document.createElement("button");
+        petActionTDButton.textContent = this.actionText;
+        petActionTD.appendChild(petActionTDButton);
+        petTR.appendChild(petActionTD);
+
+        petTR.onclick = this.showImage.bind(this);
+        petActionTDButton.onclick = this.action.bind(this);
+        petsTable.querySelector("tbody").appendChild(petTR)
+    }
 }
 
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+        this.actionText = "Meoow"
+        this.actionSoundName = "meow"
+        this.image = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop"
+    }
 }
 
-Animal.prototype.putInTheDocument = function(){
-    var petsTable = document.getElementById("petsTable");
-    var petTR = document.createElement("tr");
-  
-    var petNameTD = document.createElement("td");
-    petNameTD.textContent = this.name;
-    petTR.appendChild(petNameTD);
-  
-    var petLegsTD = document.createElement("td");
-    petLegsTD.textContent = this.legs;
-    petTR.appendChild(petLegsTD);
-  
-    var petActionTD = document.createElement("td");
-    var petActionTDButton = document.createElement("button");
-    petActionTDButton.textContent = this.actionText;
-    petActionTD.appendChild(petActionTDButton);
-    petTR.appendChild(petActionTD);
-    
-    petActionTDButton.onclick = this.action.bind(this);
-    petsTable.querySelector("tbody").appendChild(petTR)
+class Monkey extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 4;
+        this.actionText = "Scream"
+        this.actionSoundName = "scream"
+        this.image = "https://www.bostonherald.com/wp-content/uploads/2020/05/GettyImages-824860820.jpg"
+    }
 }
-
-
-function Cat(name){
-    Animal.call(this, name);
-    this.legs = 4;
-    this.actionText = "Meoow"
-    this.actionSoundName = "meow"
-}
-
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
-    this.legs = 2;
-    this.actionText = "Scream";
-    this.actionSoundName = "scream";
-}
-
-Monkey.prototype = Animal.prototype;
 
 var Mila = new Cat("Mila");
 Mila.putInTheDocument();
 
 var Charlie = new Monkey("Charlie");
 Charlie.putInTheDocument();
+  
+  
